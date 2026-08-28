7a39ad9f08aeb4d65b4cfd0bfdaa503994992334
kamsaninagalakshmi <kamsaninagalakshmi@gmail.com>
2026-08-28 23:16:51 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,16 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { fileURLToPath, URL } from 'node:url';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/weather/',
   plugins: [react()],
+
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
+
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
